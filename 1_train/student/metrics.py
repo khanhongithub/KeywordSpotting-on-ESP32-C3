79461bc671a59e3e5c9d6bf76196a64ff91cd1c0
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-11-07 12:18:20 +0100

    student/metrics.py: add missing type annotations and docstrings

--- 1_train/student/metrics.py
+++ 1_train/student/metrics.py
@@ -1,22 +1,25 @@
 # Imports
+from typing import Dict
 import tensorflow as tf
 
 # Further imports are NOT allowed, please use the APIs in `tf`, `tf.keras` and `tf.keras.backend`!
 
 
-def recall(matrix: int, idx: int) -> int:
+def recall(
+    matrix: tf.Tensor, idx: int
+) -> tf.Tensor:
     """Calclate the recall metric for a given confusion matrix and category.
 
     Arguments
     ---------
-    matrix : ?
+    matrix : tensorflow.Tensor
         The confusion matrix for the trained model. (rows: real labels, cols: predicted labels)
     idx : int
         The category index (0: silence, 1: unknown, 2: ...)
 
     Returns
     -------
-    recall : ?
+    recall : tensorflow.Tensor
         The calculated recall value (between 0 and 1)
     """
 
@@ -29,7 +32,23 @@ def recall(matrix: int, idx: int) -> int:
     return recall
 
 
-def precision(matrix, idx):
+def precision(
+    matrix: tf.Tensor, idx: int
+) -> tf.Tensor:
+    """Calclate the precision metric for a given confusion matrix and category.
+
+    Arguments
+    ---------
+    matrix : tensorflow.Tensor
+        The confusion matrix for the trained model. (rows: real labels, cols: predicted labels)
+    idx : int
+        The category index (0: silence, 1: unknown, 2: ...)
+
+    Returns
+    -------
+    recall : tensorflow.Tensor
+        The calculated precision value (between 0 and 1)
+    """
 
     precision = None
 
@@ -40,7 +59,23 @@ def precision(matrix, idx):
     return precision
 
 
-def f1_score(matrix, idx):
+def f1_score(
+    matrix: tf.Tensor, idx: int
+) -> tf.Tensor:
+    """Calclate the f1_score metric for a given confusion matrix and category.
+
+    Arguments
+    ---------
+    matrix : tensorflow.Tensor
+        The confusion matrix for the trained model. (rows: real labels, cols: predicted labels)
+    idx : int
+        The category index (0: silence, 1: unknown, 2: ...)
+
+    Returns
+    -------
+    recall : tensorflow.Tensor
+        The calculated f1_score value (between 0 and 1)
+    """
 
     f1_score = None
 
@@ -51,10 +86,12 @@ def f1_score(matrix, idx):
     return f1_score
 
 
-def get_student_metrics(matrix, idx):
-
-    return {
-        "recall": recall(matrix, idx).numpy(),
-        "precision": precision(matrix, idx).numpy(),
-        "f1_score": f1_score(matrix, idx).numpy(),
+def get_student_metrics(
+    matrix: tf.Tensor, idx
+) -> Dict[str, tf.Tensor]:
+    ret = {
+        "recall": recall(matrix, idx),
+        "precision": precision(matrix, idx),
+        "f1_score": f1_score(matrix, idx),
     }
+    return {key: value.numpy() for key, value in ret.items() if value is not None}
