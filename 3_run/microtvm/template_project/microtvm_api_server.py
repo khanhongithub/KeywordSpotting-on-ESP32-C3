02945f3d7c01b5cd696f9784f185455f250927d2
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-12-07 01:15:38 +0100

    remove print

--- 3_run/microtvm/template_project/microtvm_api_server.py
+++ 3_run/microtvm/template_project/microtvm_api_server.py
@@ -80,7 +80,6 @@ except FileNotFoundError:
 
 
 def check_call(cmd_args, *args, **kwargs):
-    print("check_call", cmd_args, args, kwargs)
     cwd_str = "" if "cwd" not in kwargs else f" (in cwd: {kwargs['cwd']})"
     _LOG.info("run%s: %s", cwd_str, " ".join(shlex.quote(a) for a in cmd_args))
     return subprocess.check_call(cmd_args, *args, **kwargs)
